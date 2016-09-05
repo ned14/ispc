76c123b6cc505c6c08abc176045a8dff8f40e208
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-09-05 15:53:05 +0100

    Support LLVM 3.9 on Windows

--- buildispc.bat
+++ buildispc.bat
@@ -4,8 +4,10 @@ REM If LLVM_INSTALL_DIR isn't set globally in your environment,
 REM it can be set here_
 REM set LLVM_INSTALL_DIR=c:\users\mmp\llvm-dev
 REM set LLVM_VERSION=LLVM_3_2
+set "LLVM_INSTALL_DIR=G:\llvm"
+set LLVM_VERSION=LLVM_3_9
 
 REM Both the LLVM binaries and python need to be in the path
-set path=%LLVM_INSTALL_DIR%\bin;%PATH%;c:\cygwin\bin
+set path=%LLVM_INSTALL_DIR%\bin;%PATH%;G:\cygwin64\bin
 
 msbuild ispc.vcxproj /V:m /p:Platform=Win32 /p:Configuration=Release
